dac67d71322897a86970c114ef31ef6954cbb4ea
Shadipto Mojumder <118707478+shadipto58@users.noreply.github.com>
2023-09-25 00:05:59 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h2 className='text-xl text-yellow-300'>This Is a Fresh APP</h2>
+      <button className="rounded-full ...">Save Changes</button>
+      <div className='container mx-auto'>
+        <img src="../public/images/profile-2.png" alt="" />
+        <img src="../public/images/profile-3.png" alt="" />
+        <img src="../public/images/profile-4.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default App
